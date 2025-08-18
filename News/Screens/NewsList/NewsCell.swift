8986aa6fd5712eb5f60fe1ecb13c938d8e68ff18
Burak Özdemir <mburak.ozdemir03@gmail.com>
2025-08-18 16:22:59 +0300

    Add: news list controller base structure completed

---
+++ News/Screens/NewsList/NewsCell.swift
@@ -0,0 +1,12 @@
+//
+//  NewsCell.swift
+//  News
+//
+//  Created by Burak Özdemir on 18.08.2025.
+//
+
+import UIKit
+
+class NewsCell: UITableViewCell {
+    static let identifier = CellType.newsCell.rawValue
+}
