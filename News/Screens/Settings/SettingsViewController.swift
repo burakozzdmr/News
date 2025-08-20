bc83d5bf58273bd6d8a64a0d42dfab14530cc348
Burak Özdemir <mburak.ozdemir03@gmail.com>
2025-08-20 10:02:20 +0300

    Add: settings controller base structure completed

--- News/Screens/Settings/SettingsViewController.swift
+++ News/Screens/Settings/SettingsViewController.swift
@@ -6,13 +6,90 @@
 //
 
 import UIKit
+import SnapKit
+
+protocol SettingsViewControllerProtocol: AnyObject {
+    
+}
+
+// MARK: - SettingsViewController
 
 class SettingsViewController: UIViewController {
 
+    // MARK: Properties
+    
+    private lazy var settingsTableView: UITableView = {
+        let tableView: UITableView = .init(frame: .zero, style: .insetGrouped)
+        tableView.dataSource = self
+        tableView.delegate = self
+        return tableView
+    }()
+
+    private let viewModel: SettingsViewModel
+    
+    // MARK: Life Cycles
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        configureView()
     }
+    
+    // MARK: Inits
+    
+    init(viewModel: SettingsViewModel = .init()) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+        
+        self.viewModel.controllerDelegate = self
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Privates
+
+private extension SettingsViewController {
+    func configureView() {
+        addViews()
+        configureLayout()
+    }
+    
+    func addViews() {
+        view.addSubview(settingsTableView)
+    }
+    
+    func configureLayout() {
+        settingsTableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension SettingsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return .init()
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension SettingsViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
+
+// MARK: - SettingsControllerProtocol
+
+extension SettingsViewController: SettingsViewControllerProtocol {
     
 }
--- News/Screens/Settings/SettingsViewModel.swift
+++ News/Screens/Settings/SettingsViewModel.swift
@@ -7,6 +7,22 @@
 
 import Foundation
 
+// MARK: - SettingsViewModelProtocol
+
+protocol SettingsViewModelProtocol: AnyObject {
+    
+}
+
 class SettingsViewModel {
+    
+    weak var viewModelDelegate: SettingsViewModelProtocol?
+    weak var controllerDelegate: SettingsViewControllerProtocol?
+    
+    init() {
+        viewModelDelegate = self
+    }
+}
+
+extension SettingsViewModel: SettingsViewModelProtocol {
     
 }
